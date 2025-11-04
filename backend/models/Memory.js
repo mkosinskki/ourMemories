f9e0bd0c80ce766a2ebba8caf014a6fa2ac98eaf
mkosinski <m.kosinskki@gmail.com>
2025-11-04 20:59:51 +0100

    Added photos to memories. Added Middleware for photos. Changed way of sending coordinates when creating memories. Linked user to created memory.

--- backend/models/Memory.js
+++ backend/models/Memory.js
@@ -4,7 +4,14 @@ const memorySchema = new mongoose.Schema({
     location: { type: mongoose.Schema.Types.ObjectId, ref: "Location", required: [ true, "Location is required" ]},
     eventDate: { type: Date, required: [ true, "Event date is required" ]},
     description: { type: String, required: [ true, "Description is required" ]},
-    timestamp: { type: Date, default: Date.now }
+    timestamp: { type: Date, default: Date.now },
+    photos: { type: [String], required: [true, "At least one photo is required"], validate: {
+        validator: function(v) { 
+            return v.length > 0
+        }, 
+        message: "At least one photo is required" 
+    } },
+    user: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: [true, "Owner of memory is required" ]}
 })
 
 const Memory = mongoose.models.Memory || mongoose.model("Memory", memorySchema);
--- backend/routes/memoriesRouter.js
+++ backend/routes/memoriesRouter.js
@@ -1,11 +1,11 @@
 import express from 'express';
 import { getMemories, addMemory } from '../controllers/memoriesController.js';
 import { protect, authorize } from '../middleware/authMiddleware.js';
+import { uploadMemoryPhotos } from '../middleware/uploadMiddleware.js';
 
 const router = express.Router();
 
-// router.get('/', getMemories);
 router.get('/', protect, authorize(1), getMemories);
-router.post('/', protect, authorize(1), addMemory);
+router.post('/', protect, authorize(1), uploadMemoryPhotos, addMemory);
 
 export default router;
