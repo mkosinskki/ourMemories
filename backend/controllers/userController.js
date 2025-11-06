733918d52185d0da91838aabbb861878ca36c163
mkosinski <m.kosinskki@gmail.com>
2025-11-06 22:20:40 +0100

    ProfilePage.vue - frontend. Added number of memories and number of locations added by user (returned in userController for use in statistics in ProfilePage.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,45 +1,64 @@
-import User from '../models/User.js'
-import mongoose from 'mongoose';
+import User from "../models/User.js";
+import Memory from "../models/Memory.js";
+import mongoose from "mongoose";
 
-
-export const getUser = (req, res) => {
+export const getUser = async (req, res) => {
   const user = req.user;
 
   try {
-    const userToReturn = {
-            _id: user._id,
-            email: user.email,
-            firstName: user.firstName,
-            surname: user.surname,
-            dateOfBirth: user.dateOfBirth,
-            role: user.role
-        };
+    const postCount = await Memory.countDocuments({ user: user._id });
 
-        res.status(200).json(userToReturn);
+    const uniqueLocationIds = await Memory.distinct("location", {
+      user: user._id,
+    });
+    const locationCount = uniqueLocationIds.length;
+
+    const userToReturn = {
+      _id: user._id,
+      email: user.email,
+      firstName: user.firstName,
+      surname: user.surname,
+      dateOfBirth: user.dateOfBirth,
+      role: user.role,
+      postCount: postCount,
+      locationCount: locationCount,
+    };
+
+    res.status(200).json(userToReturn);
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error." });
   }
-  catch {
-    res.status(500).json({ error: 'Internal server error.' });
-  }
-}
+};
 
 export const getUserById = async (req, res) => {
   try {
     const { id } = req.params;
 
     if (!mongoose.Types.ObjectId.isValid(id)) {
-      return res.status(400).json({ message: 'Incorrect id format.' });
+      return res.status(400).json({ message: "Incorrect id format." });
     }
 
-    const user = await User.findById(id).select('_id email firstName surname dateOfBirth role');
+    const user = await User.findById(id).select(
+      "_id email firstName surname dateOfBirth role"
+    );
 
     if (!user) {
-      return res.status(404).json({ message: 'No user with specified ID was found.' });
+      return res
+        .status(404)
+        .json({ message: "No user with specified ID was found." });
     }
 
-    res.status(200).json(user);
+    const postCount = await Memory.countDocuments({ user: id });
 
-  } 
-  catch (error) {
-    res.status(500).json({ message: 'Internal server error.' });
+    const uniqueLocationIds = await Memory.distinct("location", { user: id });
+    const locationCount = uniqueLocationIds.length;
+
+    const userProfile = user.toObject();
+    userProfile.postCount = postCount;
+    userProfile.locationCount = locationCount;
+
+    res.status(200).json(userProfile);
+  } catch (error) {
+    res.status(500).json({ message: "Internal server error." });
   }
 };
